dfbcdf82b6a616795a4dc71397d37bf2bd719a52
K-Rostislav <moregan123456789@gmail.com>
2023-03-24 23:31:18 +0300

    fix

--- src/assets/js/vendor/parallax.js
+++ src/assets/js/vendor/parallax.js
@@ -1,12 +1,14 @@
 import Ukiyo from "ukiyojs"
 
-if (window.innerWidth > 1024) {
-    new Ukiyo('#img', {
-        speed: 1.65,
-        willChange: true,
-    })
-    new Ukiyo('#bg', {
-        speed: 1.9,
-        willChange: true,
-    })
-}
+window.addEventListener('DOMContentLoaded', () => {
+    if (window.innerWidth > 1024) {
+        new Ukiyo('#img', {
+            speed: 1.65,
+            willChange: true,
+        })
+        new Ukiyo('#bg', {
+            speed: 1.9,
+            willChange: true,
+        })
+    }
+})
